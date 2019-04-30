fbfb853d2da2620c1aff7db0dd2f53c066ab8b6c
ahmadnaeem123 <ahmadnaeem@ucp.edu.pk>
2019-04-30 11:00:55 +0500

    lec 15

--- lec 15/script.js
+++ lec 15/script.js
@@ -8,4 +8,94 @@ https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
 b1.addEventListener("mouseenter",function () {
     console.log("mouse entered !")
 })*/
+var button = document.getElementById("enter");
+var input = document.getElementById("user-input");
+var ul = document.getElementsByTagName("ul")[0];
+console.log(button) ;
+
+function inputlength(){
+      if(input.value.length>0) {
+          return true;
+      }
+      else {
+          return false;
+      }
+
+}
+  function listitem()
+  {
+          var li = document.createElement("li");
+          li.append(document.createTextNode(input.value));
+          ul.append(li);
+          input.value='';
+  }
+
+  function additemafterclick()
+  {          if(inputlength()) {
+                  listitem();
+
+
+
+  }
+  }
+
+  function additemafterpress(e) {
+      if (inputlength() && e.which === 13) {
+          listitem();
+      }
+  }
+   function isdone()
+   {
+
+       this.classList.toggle("done");
+
+   }
+
+
+button.addEventListener("click",additemafterclick);
+input.addEventListener("keypress",additemafterpress);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
