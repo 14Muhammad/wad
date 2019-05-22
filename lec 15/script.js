19470757d3f444c5fe4523e9d120881fa01a1331
Asad Ullah Awan <asadullah.awan@ucp.edu.pk>
2019-05-22 10:19:38 +0500

    Merge remote-tracking branch 'remotes/upstream/master'
    # Conflicts:
    #	lec 09/robot/style.css

--- lec 15/script.js
+++ lec 15/script.js
@@ -9,3 +9,39 @@ b1.addEventListener("mouseenter",function () {
     console.log("mouse entered !")
 })*/
 
+var button = document.getElementById("enter");
+var ul = document.getElementsByTagName("ul")[0];
+var input = document.getElementById("user-input");
+
+function inputLenght()
+{
+    if(input.value.length>0)
+        return true;
+        return false;
+}
+function createListElement()
+{
+    var li = document.createElement("li");
+    li.append(document.createTextNode(input.value));
+    ul.append(li);
+    input.value = '';
+}
+// console.log(button);
+button.addEventListener("click",function(){
+  if(inputLenght())
+  createListElement();
+});
+input.addEventListener("keypress",function(e){
+    if(inputLenght()& e.which===13)
+       createListElement();
+});
+function addEvenOnItems()
+{
+    for(vari=0;i<listItems.length;i++) {
+        listItems[i].addEventListener("click", done)
+    }
+}
+function done()
+{
+    this.classList.toggle("done");
+}
