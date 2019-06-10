bee2395d014c4c6a08036d989b53b7e6fef80152
Ali Makhdoom <14muhammad@gmail.com>
2019-06-10 09:38:36 +0500

    insert pro php

--- pro/admin/insert_product.php
+++ pro/admin/insert_product.php
@@ -1,4 +1,30 @@
 <!DOCTYPE html>
+<?php
+$con = mysqli_connect("localhost","root","","tech_box_db");
+if(!$con)
+    die("Connection failed");
+?>
+<?php
+
+if(isset($_POST['insert_pro'])){
+    //getting text data from the fields
+    $pro_title = $_POST['pro_title'];
+    $pro_cat = $_POST['pro_cat'];
+    $pro_brand = $_POST['pro_brand'];
+    $pro_price = $_POST['pro_price'];
+    $pro_desc = $_POST['pro_desc'];
+    $pro_keywords = $_POST['pro_keywords'];
+
+
+
+    $insert_product = "insert into products (pro_cat, pro_brand,pro_title,pro_price,pro_desc,pro_image,pro_keywords) 
+                  VALUES ('$pro_cat','$pro_brand','$pro_title','$pro_price','$pro_desc','$pro_image','$pro_keywords');";
+    $insert_pro = mysqli_query($con, $insert_product);
+    if($insert_pro){
+        header("location: ".$_SERVER['PHP_SELF']);
+    }
+}
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -18,7 +44,7 @@
 <div class="container-fluid">
     <h1 class="text-center my-4"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span>
         Product </h1>
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="" method="post">
         <div class="row">
             <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
                 <label for="pro_title" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span>
@@ -44,6 +70,15 @@
                     </div>
                     <select class="form-control" id="pro_cat" name="pro_cat">
                         <option>Select Category</option>
+                        <?php
+                            $getCatsQuery = "select * from categories";
+                            $getCatsResult = mysqli_query($con,$getCatsQuery);
+                            while($row = mysqli_fetch_assoc($getCatsResult)){
+                                $cat_id = $row['cat_id'];
+                                $cat_title = $row['cat_title'];
+                                echo "<option value='$cat_id'>$cat_title</option>";
+                            }
+                        ?>
                     </select>
                 </div>
             </div>
@@ -60,6 +95,15 @@
                     </div>
                     <select class="form-control" id="pro_brand" name="pro_brand">
                         <option>Select Brand</option>
+                        <?php
+                            $getBrandsQuery = "select * from brands";
+                            $getBrandsResult = mysqli_query($con,$getBrandsQuery);
+                            while($row = mysqli_fetch_assoc($getBrandsResult)){
+                                $brand_id = $row['brand_id'];
+                                $brand_title = $row['brand_title'];
+                                echo "<option value='$brand_id'>$brand_title</option>";
+                            }
+                            ?>
                     </select>
                 </div>
             </div>
