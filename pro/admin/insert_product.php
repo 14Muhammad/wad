73a311d2de91ccd5a52972dfb75d643181790e9c
Ali Makhdoom <14muhammad@gmail.com>
2019-06-11 15:27:28 +0500

    move and insert image code

--- pro/admin/insert_product.php
+++ pro/admin/insert_product.php
@@ -15,10 +15,13 @@ if(isset($_POST['insert_pro'])){
     $pro_desc = $_POST['pro_desc'];
     $pro_keywords = $_POST['pro_keywords'];
 
+    //getting image from the field
+    $pro_image = $_FILES['pro_image']['name'];
+    $pro_image_tmp = $_FILES['pro_image']['tmp_name'];
+    move_uploaded_file($pro_image_tmp,"product_images/$pro_image");
 
-
-    $insert_product = "insert into products (pro_cat, pro_brand,pro_title,pro_price,pro_desc,pro_keywords) 
-                  VALUES ('$pro_cat','$pro_brand','$pro_title','$pro_price','$pro_desc','$pro_keywords');";
+    $insert_product = "insert into products (pro_cat, pro_brand,pro_title,pro_price,pro_desc,pro_keywords,pro_image) 
+                  VALUES ('$pro_cat','$pro_brand','$pro_title','$pro_price','$pro_desc','$pro_keywords','$pro_image');";
     $insert_pro = mysqli_query($con, $insert_product);
     if($insert_pro){
         header("location: ".$_SERVER['PHP_SELF']);
@@ -44,7 +47,7 @@ if(isset($_POST['insert_pro'])){
 <div class="container-fluid">
     <h1 class="text-center my-4"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span>
         Product </h1>
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         <div class="row">
             <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
                 <label for="pro_title" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span>
--- pro/database/tech_box_db.sql
+++ pro/database/tech_box_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2019 at 07:19 AM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.6
+-- Generation Time: Jun 11, 2019 at 12:26 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -78,21 +78,26 @@ INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
 
 CREATE TABLE `products` (
   `pro_id` int(10) NOT NULL,
-  `pro_title` varchar(255) NOT NULL,
-  `pro_brand` int(10) NOT NULL,
   `pro_cat` int(10) NOT NULL,
-  `pro_price` varchar(255) NOT NULL,
-  `pro_image` varchar(255) NOT NULL,
-  `pro_keywords` varchar(255) NOT NULL,
-  `pro_desc` varchar(255) NOT NULL
+  `pro_brand` int(10) NOT NULL,
+  `pro_title` varchar(200) NOT NULL,
+  `pro_price` int(10) NOT NULL,
+  `pro_desc` varchar(2000) NOT NULL,
+  `pro_image` varchar(300) NOT NULL,
+  `pro_keywords` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`pro_id`, `pro_title`, `pro_brand`, `pro_cat`, `pro_price`, `pro_image`, `pro_keywords`, `pro_desc`) VALUES
-(1, 'Samsung Note 6', 3, 10, '90000', '', 'samsung', 'Good');
+INSERT INTO `products` (`pro_id`, `pro_cat`, `pro_brand`, `pro_title`, `pro_price`, `pro_desc`, `pro_image`, `pro_keywords`) VALUES
+(5, 2, 1, 'apple Mac Book', 323000, '                        i9 , 16 GB RAM                                           ', '71KyjhnQY4L._SY355_.jpg', '321000'),
+(6, 1, 2, 'dell inpiron', 50000, 'dell is good laptop', 'dell1.jpg', 'dell, inspiron'),
+(10, 10, 1, 'iPhone 7', 90000, 'Apple product', '71x3e0x+M2L._SX569_.jpg', 'apple phone, iphone 7'),
+(11, 7, 7, 'Sony Alpha a58', 59900, 'Sony is Good Company', 'song1.jpg', 'sony , camera, DSLR'),
+(12, 6, 3, 'Samsung Gear S3', 20000, 'Samsung product', 'samsung2 (2).jpg', 'samsung, watch'),
+(13, 10, 6, 'oppo f1s', 25000, 'Oppo phone', 'QMobile-Noir-i8i-Techjuice.jpg', 'oppo');
 
 --
 -- Indexes for dumped tables
@@ -136,7 +141,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `pro_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `pro_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
