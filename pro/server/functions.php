d194f54b5dd8d6534bd924ca095227314cc61daa
Ali Makhdoom <14muhammad@gmail.com>
2019-06-11 16:22:28 +0500

    separation of PHP Code in function.php

---
+++ pro/server/functions.php
@@ -0,0 +1,26 @@
+<?php
+require_once "db_connection.php";
+
+function getCats(){
+    global $con;
+    $getCatsQuery = "select * from categories";
+    $getCatsResult = mysqli_query($con,$getCatsQuery);
+    while($row = mysqli_fetch_assoc($getCatsResult)){
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+        echo "<li><a class='nav-link'  href='#'>$cat_title</a></li>";
+    }
+}
+function getBrands(){
+    global $con;
+    $getBrandsQuery = "select * from brands";
+    $getBrandsResult = mysqli_query($con,$getBrandsQuery);
+    while($row = mysqli_fetch_assoc($getBrandsResult)){
+        $brand_id = $row['brand_id'];
+        $brand_title = $row['brand_title'];
+        echo "<li><a class='nav-link'  href=''#'>$brand_title</a></li>";
+    }
+}
+
+
+
