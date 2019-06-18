ba553e42ac81eaa0259d72236035350d19bd15eb
Ali Makhdoom <14muhammad@gmail.com>
2019-06-19 02:04:20 +0500

    logout page

--- pro/database/tech_box_db.sql
+++ pro/database/tech_box_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2019 at 08:37 PM
+-- Generation Time: Jun 18, 2019 at 11:01 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -93,6 +93,32 @@ INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `cust_id` int(10) NOT NULL,
+  `cust_ip` varchar(255) NOT NULL,
+  `cust_name` varchar(100) NOT NULL,
+  `cust_email` varchar(100) NOT NULL,
+  `cust_pass` varchar(100) NOT NULL,
+  `cust_country` varchar(100) NOT NULL,
+  `cust_city` varchar(100) NOT NULL,
+  `cust_contact` varchar(100) NOT NULL,
+  `cust_address` varchar(255) NOT NULL,
+  `cust_image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`cust_id`, `cust_ip`, `cust_name`, `cust_email`, `cust_pass`, `cust_country`, `cust_city`, `cust_contact`, `cust_address`, `cust_image`) VALUES
+(7, '::1', 'Muhammad Ali', 'ali@ucp', '123', 'Pakistan', 'Lahore', '03314527994', 'Iqbal Town', 'My Pic.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -146,6 +172,12 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`cat_id`);
 
 --
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`cust_id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -172,6 +204,12 @@ ALTER TABLE `brands`
 --
 ALTER TABLE `categories`
   MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `cust_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `products`
